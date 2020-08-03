403f5ce020d1bc1f6c0928281412b87ffdf422b3
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-08-03 14:42:49 -0500

    ⚔️ POST 'api/users' -- add a user

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ server.get("/hello", (req, res) => {
 
 let users = [
   {
-    id: nanoid(), // hint: use the shortid npm package to generate it
+    id: nanoid(5), // hint: use the shortid npm package to generate it
     name: "Jane Doe", // String, required
     bio: "Not Tarzan's Wife, another Jane", // String, required
+  },
+  {
+    id: nanoid(5), // hint: use the shortid npm package to generate it
+    name: "Bob Ross", // String, required
+    bio: "Painting is FUN", // String, required
   },
 ];
 
 // GET all users
 server.get("/api/users", (req, res) => {
   // Returns an array users
-  res.status(200).send(users);
+  res.status(200).json(users);
 });
 
 // GET user (from ID)
@@ -29,22 +34,40 @@ server.get("/api/users/:id", (req, res) => {
   // tries to find user
   const foundUser = users.find((user) => user.id === id);
   // Returns the user object if foundUser is defined
+  // --> if undefined ... "user not found"
   foundUser
-    ? res.status(200).send(foundUser)
+    ? res.status(200).json(foundUser)
     : res.status(200).send("user not found");
 });
 
-// POST user
+// POST user (requires NAME & BIO)
 server.post("/api/users", (req, res) => {
   // Creates a user using the information sent inside the request body
+  const { name, bio } = req.body;
+  const newUser = { id: nanoid(5), name, bio };
+  // add user if name & bio are defined
+  // --> if either is undefined... status 400 "please provide.."
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    users.push(newUser);
+    res.status(201).json(newUser);
+  }
 });
 
 // DELETE user (from ID)
 server.delete("/api/users/:id", (req, res) => {
+  // save id from url ^^
+  const id = req.params.id;
+  // tries to find user
+  const foundUser = users.find((user) => user.id === id);
   // 1. removes the user with the specified id
   // 2. returns the deleted user
 });
 
+// UPDATE user
 server.put("/api/users/:id", (req, res) => {
   // 1. updates the user with the specified id using data from the request body
   // 2. returns the modified user
